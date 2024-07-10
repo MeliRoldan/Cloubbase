2485ad621f24986f1fe92562cf0293c0ade281a3
MeliRoldan <meli.rww@gmail.com>
2024-07-10 13:49:21 +0400

    Languages added
    Deleted test files, added language switcher

--- js.js
+++ js.js
@@ -1,12 +1,4 @@
-var myNav = document.getElementById("nav");
-window.onscroll = function() {
-  "use strict";
-  if (document.body.scrollTop >= 155 || document.documentElement.scrollTop >= 155) {
-    myNav.classList.add("scroll");
-  } else {
-    myNav.classList.remove("scroll");
-  }
-};
+
 
 
 $(document).ready(function(){
